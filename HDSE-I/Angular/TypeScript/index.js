dd5d5fe62f3a78376926fac5b5dac40c78983eef
farazinam <farazinam17@gmail.com>
2025-09-12 11:04:09 +0500

    Angular (TypeScript) - Day 1

---
+++ HDSE-I/Angular/TypeScript/index.js
@@ -0,0 +1,16 @@
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Batch 2408B2");
+console.log("Batch 2408B2");
+console.log("Batch 2408B2");
+document.write("Aptech Learning");
+document.write("Aptech Learning");
+document.write("Aptech Learning");
+var naam = "Bilal";
+// naam = 123;
+console.log(naam);
+console.log(naam);
+console.log(naam);
+var data = "Any Type Of Data";
+data = 123;
+console.log(data);
---
+++ HDSE-I/Angular/TypeScript/index.ts
@@ -0,0 +1,18 @@
+console.log("Hello world!");
+console.log("Hello world!");
+console.log("Batch 2408B2");
+console.log("Batch 2408B2");
+console.log("Batch 2408B2");
+document.write("Aptech Learning");
+document.write("Aptech Learning");
+document.write("Aptech Learning");
+
+var naam:string = "Bilal";
+// naam = 123;
+console.log(naam);
+console.log(naam);
+console.log(naam);
+
+var data: any = "Any Type Of Data";
+data = 123;
+console.log(data);
