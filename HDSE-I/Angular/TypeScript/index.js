dcc2c519cec42bd02c76a0e28e7785824a9e2b14
Faraz-Inam <157343724+Faraz-Inam@users.noreply.github.com>
2025-09-15 11:05:38 +0500

    Angular - Day 2

--- HDSE-I/Angular/TypeScript/index.js
+++ HDSE-I/Angular/TypeScript/index.js
@@ -1,11 +1,12 @@
-console.log("Hello world!");
-console.log("Hello world!");
-console.log("Batch 2408B2");
-console.log("Batch 2408B2");
-console.log("Batch 2408B2");
-document.write("Aptech Learning");
-document.write("Aptech Learning");
-document.write("Aptech Learning");
+// Day 1 - TypeScript
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Batch 2408B2");
+// console.log("Batch 2408B2");
+// console.log("Batch 2408B2");
+// document.write("Aptech Learning");
+// document.write("Aptech Learning");
+// document.write("Aptech Learning");
 var naam = "Bilal";
 // naam = 123;
 console.log(naam);
@@ -14,3 +15,49 @@ console.log(naam);
 var data = "Any Type Of Data";
 data = 123;
 console.log(data);
+// Day 2 - TypeScript
+var isEligible = true;
+var anydata1 = "Faraz";
+var anydata2 = 123;
+var anydata3 = 34.8;
+//array
+var arr1 = ["1", "3", "5", "7", "9"];
+var arr2 = [1, 3, 5, 7, 9];
+var arr3 = [true, false];
+//tuple
+var tup1 = ["Ali", 23, "Karachi", true];
+//object
+// var obj1 = {name: "Bilal", age :22};
+var obj1 = { name: "Bilal", age: 22 };
+var al2 = { brand: "Toyota", year: 2024 };
+console.log(al2);
+//enum
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var myColor = Color.Blue;
+console.log(myColor);
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Med"] = 2] = "Med";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+;
+var mySize = Size.Small;
+console.log(mySize);
+//function
+function abc1(a, b) {
+    console.log(a + b);
+}
+abc1(1, 7);
+function abc2(a, b) {
+    return a + b;
+}
+console.log(abc2("Faraz", " Inam"));
+var output = abc2("Muhammad", " Imran");
+console.log(output);
--- HDSE-I/Angular/TypeScript/index.ts
+++ HDSE-I/Angular/TypeScript/index.ts
@@ -1,11 +1,13 @@
-console.log("Hello world!");
-console.log("Hello world!");
-console.log("Batch 2408B2");
-console.log("Batch 2408B2");
-console.log("Batch 2408B2");
-document.write("Aptech Learning");
-document.write("Aptech Learning");
-document.write("Aptech Learning");
+// Day 1 - TypeScript
+
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Batch 2408B2");
+// console.log("Batch 2408B2");
+// console.log("Batch 2408B2");
+// document.write("Aptech Learning");
+// document.write("Aptech Learning");
+// document.write("Aptech Learning");
 
 var naam:string = "Bilal";
 // naam = 123;
@@ -16,3 +18,58 @@ console.log(naam);
 var data: any = "Any Type Of Data";
 data = 123;
 console.log(data);
+
+
+// Day 2 - TypeScript
+
+var isEligible:boolean = true;
+
+var anydata1:any = "Faraz";
+var anydata2:any = 123;
+var anydata3:any = 34.8;
+
+//array
+var arr1:string[] = ["1", "3", "5", "7", "9"];
+var arr2:number[] = [1, 3, 5, 7, 9];
+var arr3:Array<boolean> = [true, false];
+
+//tuple
+var tup1:[string, number, string, boolean] = ["Ali", 23, "Karachi", true];
+
+//object
+// var obj1 = {name: "Bilal", age :22};
+var obj1:{name:string, age:number} = {name: "Bilal", age :22};
+
+//Alias
+type al1 = {brand:string, year:number};
+
+var al2 : al1 = {brand: "Toyota", year: 2024}; 
+console.log(al2);
+
+//enum
+enum Color {Red, Green, Blue};
+let myColor : Color = Color.Blue;
+console.log(myColor);
+
+enum Size {Small = 1, Med, Large};
+let mySize : Size = Size.Small;
+console.log(mySize);
+
+//function
+function abc1(a:number, b:number){
+    console.log(a+b);
+}
+abc1(1, 7);
+
+function abc2(a:string, b:string):string{
+   return a+b;
+}
+
+console.log(abc2("Faraz", " Inam"));
+var output = abc2("Muhammad", " Imran");
+console.log(output);
+
+
+
+
+
