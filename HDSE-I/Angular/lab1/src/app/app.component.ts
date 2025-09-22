1f8a2de2a43f676902aea42cc9a6565962085013
farazinam <farazinam17@gmail.com>
2025-09-22 11:07:37 +0500

    Angular - Day 3 to 5

---
+++ HDSE-I/Angular/lab1/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [ContactComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'lab1';
+}
