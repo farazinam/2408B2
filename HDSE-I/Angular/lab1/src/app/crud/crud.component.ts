aeb202f868d8c71c7683119dafe21804850cbbc4
farazinam <farazinam17@gmail.com>
2025-10-03 11:10:52 +0500

    Angular - Day 10 (Last)

--- HDSE-I/Angular/lab1/src/app/crud/crud.component.ts
+++ HDSE-I/Angular/lab1/src/app/crud/crud.component.ts
@@ -10,13 +10,37 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class CrudComponent {
 
-  constructor(private  http : HttpClient) {}
 
+  //CREATE
+  
+  constructor(private  http : HttpClient) {}
+  
   createData(rec: {pn:string, pp:number, pd:string}){
     console.log(rec);
-    this.http.post("https://angular2408b2-default-rtdb.firebaseio.com/abc.json",rec)
+    this.http.post("https://angular2408b2-default-rtdb.firebaseio.com/products.json",rec)
     .subscribe((res : any) => {
       console.log(res); 
+    })
+  }
+  
+
+
+
+  // READ
+
+  fetch : any = [];
+
+  readData(){
+    this.http.get<{[key : string] : any}>("https://angular2408b2-default-rtdb.firebaseio.com/products.json")
+    .subscribe((res) => {
+      this.fetch = [];
+      for(let key in res){
+        if(res.hasOwnProperty(key)){
+          this.fetch.push({id: key, ...res[key]})
+        }
+      }
+      console.log(this.fetch);
+      
     })
   }
 }
