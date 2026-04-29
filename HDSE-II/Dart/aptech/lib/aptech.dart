ff2a275c802953285591acfcea5fe3a533053d3f
farazinam <farazinam17@gmail.com>
2026-04-29 11:20:43 +0500

    conditional statements, loops, and functions

--- HDSE-II/Dart/aptech/lib/aptech.dart
+++ HDSE-II/Dart/aptech/lib/aptech.dart
@@ -4,59 +4,224 @@ int calculate() {
   return 6 * 7;
 }
 void main(){
-  print("Hello dart Project");
+//   print("Hello dart Project");
 
-  //Variables
+//   //Variables
 
-  //var
-  var n = 'Faraz Inam';
-  n = 'Aptech';
-  var age = 21;
-  var isEligible = true;
+//   //var
+//   var n = 'Faraz Inam';
+//   n = 'Aptech';
+//   var age = 21;
+//   var isEligible = true;
 
-  // print(n);
-  // print(age);
-  // print(isEligible);
+//   // print(n);
+//   // print(age);
+//   // print(isEligible);
 
-  print('$n \n$age \n$isEligible');
+//   print('$n \n$age \n$isEligible');
 
-//const
-  const pI = 3.14;
-  // PI = 2.2;
-  print(pI);
-  // const date1 = DateTime.now();
-  // print(date1);
+// //const
+//   const pI = 3.14;
+//   // PI = 2.2;
+//   print(pI);
+//   // const date1 = DateTime.now();
+//   // print(date1);
 
 
-//final
-  final num = 1234;
-  // num = 4321;
-  print(num);
-  final date2 = DateTime.now();
-  print(date2);
+// //final
+//   final num = 1234;
+//   // num = 4321;
+//   print(num);
+//   final date2 = DateTime.now();
+//   print(date2);
 
-  // //late
-   late String lab22;
-   lab22 = "2408B2";
+//   // //late
+//    late String lab22;
+//    lab22 = "2408B2";
 
 
   
-  //Type Casting
-  int rollNo = 123;
-  double height = 12.3;
-  double width = 12;
-  String lab = 'lab22';
-  bool isAllowed = true;
+//   //Type Casting
+//   int rollNo = 123;
+//   double height = 12.3;
+//   double width = 12;
+//   String lab = 'lab22';
+//   bool isAllowed = true;
 
-  print('$rollNo \n $height \n $width \n $lab \n $isAllowed');
+//   print('$rollNo \n $height \n $width \n $lab \n $isAllowed');
 
-  dynamic city = "Karachi";
-  print(city);
+//   dynamic city = "Karachi";
+//   print(city);
 
-  //Taking input from User
+//   //Taking input from User
 
-print("Enter Your Name:");
-  String? yourName = stdin.readLineSync();
-  print('Your Name is $yourName');
-  stdout.write('Your Name is $yourName');
+// print("Enter Your Name:");
+//   String? yourName = stdin.readLineSync();
+//   print('Your Name is $yourName');
+//   stdout.write('Your Name is $yourName');
+
+
+// Operators
+// Uniary Operators
+  // inc and dec
+
+// Binary Operators
+// Arithmetic
+// Logical
+// Assignment
+// Comparison
+
+// Ternary
+// print("Enter Your age :");
+// var age = int.parse(stdin.readLineSync()!);
+// var checkage =  (age >= 18) ? "You are Eligible" : "Not Eligible";
+// print(checkage);
+
+//Null Coallescing Operator
+// String? name = "Ali";
+// String checkName = name ?? "Guest";
+// print(checkName);
+
+//Type Test operator
+// String city = "Islamabad";
+// print(city is !int);
+
+
+//Conditional Statement
+// print("Enter Your age :");
+// var age = int.parse(stdin.readLineSync()!);
+
+// if(age >= 18){
+//   print("You are eligible");
+// }
+// else{
+//   print("You are NOT Eligible");
+// }
+
+//Nested if else
+// if(){
+//   if(){
+//     else{
+
+//     }
+//   }
+// }
+// else{
+
+// }
+
+//Multiple Ifs
+// if(){
+
+// }
+// if(){
+
+// }
+
+//if else (ladder)
+// if(){
+
+// }
+// else if(){
+
+// }
+// else{
+
+// }
+
+//Switch
+// String vegetable = "tomato";
+// switch(vegetable){
+//   case "tomato":
+//   print("Yes $vegetable are Availabe");
+//   break;
+//   case "potato":
+//   print("Yes $vegetable are Availabe");
+//   break;
+//   default:
+//   print("$vegetable is NOT availabe");
+// }
+
+
+//Loops
+
+//counting
+// for(int i = 1; i <= 10; i++){
+//    print(i);
+// }
+
+//even and odd numbers
+// for(int i = 0; i <= 20; i = i+2){
+//    print(i);
+// }
+// for(int i = 1; i <= 20; i = i+2){
+//    print(i);
+// }
+
+//int i;
+// for(i = 0; i <= 20; i++){
+//   if(i%2 == 0){
+//    print(i);
+//   }
+// }
+
+// for(int i = 0; i <= 20; i++){
+//   if(i%2 == 0){
+//     continue;
+//   }
+//    print(i);
+// }
+
+// print("Enter a Number :");
+// int num = int.parse(stdin.readLineSync()!);
+// int i = 1;
+// while(i <= 10){
+//   print("$num X $i = ${i*num}");
+// i++;
+// }
+
+// int num = 1;
+// while(num < 1){
+//   print(num);
+//   num++;
+// }
+
+// int d = 1;
+// do{
+//   print(d);
+//   d++;
+// }
+// while(d < 1);
+
+// Function
+
+//Custom Function
+
+// simpleFn(){
+//   print("this is Simple Function");
+// }
+
+// simpleFn();
+
+// paraFn(String fn, String ln){
+//   print(fn + ln);
+// }
+
+// paraFn("Faraz", " Inam");
+
+// paraFn2(int num1, int num2){
+//   print(num1 + num2);
+// }
+
+// paraFn2(3, 9);
+
+// DefParaFn([name = "Guest"]){
+//   print(name);
+// }
+
+// // DefParaFn();
+// DefParaFn("Aptech");
+
+
+
 }
