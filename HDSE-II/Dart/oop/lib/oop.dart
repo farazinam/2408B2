396e423ca83e0b3daaa8f6c720860d4f3872551b
farazinam <farazinam17@gmail.com>
2026-05-08 15:05:33 +0500

    Access Modifiers and Constructor

--- HDSE-II/Dart/oop/lib/oop.dart
+++ HDSE-II/Dart/oop/lib/oop.dart
@@ -1,25 +1,49 @@
+import 'dart:io';
+
 int calculate() {
   return 6 * 7;
 }
 
 //class
-class Car{
-  String Model = "";
-  String Color = "";
-  int year = 0;
+//  class _Car{    // (_) is used for private class
+//   String Model = "";
+//   String Color = "";
+//   int year = 0;
 
-  running(){
-    print("Car Model is $Model, Color is $Color, year is $year");
+//   running(){
+//     print("Car Model is $Model, Color is $Color, year is $year");
+//   }
+// }
+
+class Person{
+  String Enrollment = "";
+  String Email = "";
+  int age = 0;
+
+  Person(this.Enrollment, this.Email, this.age);
+
+  details(){
+    print("Enrollment is $Enrollment, Email is $Email, Age is $age");
   }
 }
 
 void main(){
-  print("Welcome to OOP in Dart");
+  // print("Welcome to OOP in Dart");
 
-  Car c = Car();
-  c.Color = "Black";
-  c.Model = "abc123";
-  c.year = 2024;
+  // _Car c = _Car();
+  // c.Color = "Black";
+  // c.Model = "abc123";
+  // c.year = 2024;
 
-  c.running();
+  // c.running();
+
+  print("Enter Enrollment Number: ");
+  String enroll = stdin.readLineSync()!;
+  print("Enter Email : ");
+  var email = stdin.readLineSync()!;
+  print("Enter Age: ");
+  var age = int.parse(stdin.readLineSync()!);
+  
+  Person p = Person(enroll, email, age);
+  p.details();
 }
