844e5f369e1e345fc98d9761e43a747460d153ac
farazinam <farazinam17@gmail.com>
2026-05-25 11:21:35 +0500

    AppBar completed with Drawer along with Rows() and Column()

--- HDSE-II/Flutter/mobileapp/lib/main.dart
+++ HDSE-II/Flutter/mobileapp/lib/main.dart
@@ -6,25 +6,154 @@ void main(){
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Watch"),
+          title: Text("AppBar"),
           centerTitle: true,
           backgroundColor: Colors.amber,
-          leading: Icon(Icons.menu),
           actions: [
             IconButton(onPressed: (){
               print("You Clicked Button");
-            }, icon: Icon(Icons.login))
+            }, 
+            icon: Icon(Icons.message))
           ],
         ),
-        body: Column(
-          children: [
-            Container(
-              child: Text("This is s body"),
-              color: Colors.blueAccent,
-              width: 200,
-            )
-          ],
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              UserAccountsDrawerHeader(
+                accountName: Text("Faraz Inam"), 
+                accountEmail: Text("farazinam17@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  child: Text("FI"),
+                ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text("Settings"),
+                  subtitle: Text("Change your settings"),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+                ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text("Settings"),
+                  subtitle: Text("Change your settings"),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+                ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text("Settings"),
+                  subtitle: Text("Change your settings"),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+                ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text("Settings"),
+                  subtitle: Text("Change your settings"),
+                  trailing: Icon(Icons.arrow_forward),
+                ),
+            ],
+          ),
         ),
+        body:
+        // SingleChildScrollView(
+        //   child: 
+        //  Row(
+        //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //   children: [ 
+        //     Container(
+        //       // margin: EdgeInsets.all(20),
+        //       margin: EdgeInsets.only(top: 20, left: 20),
+        //       // margin: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+        //       padding: EdgeInsets.all(40),
+        //       child: Text("This is Container 1", style: TextStyle(color: Colors.white),),
+        //       color: Colors.blueAccent,
+        //       width: 200,
+        //       height: 100,
+        //     ),
+        //     Container(
+        //       margin: EdgeInsets.only(top: 20),
+        //       padding: EdgeInsets.all(40),
+        //       child: Text("This is Container 2", style: TextStyle(color: Colors.white),),
+        //       color: Colors.orange,
+        //       width: 200,
+        //       height: 100,
+        //     ),
+        //     Container(
+        //       margin: EdgeInsets.only(top: 20),
+        //       padding: EdgeInsets.all(40),
+        //       child: Text("This is Container 3", style: TextStyle(color: Colors.white),),
+        //       color: Colors.grey,
+        //       width: 200,
+        //       height: 100,
+        //     ),
+        //     Container(
+        //       margin: EdgeInsets.only(top: 20),
+        //       padding: EdgeInsets.all(40),
+        //       child: Text("This is Container 4", style: TextStyle(color: Colors.white),),
+        //       color: Colors.purple,
+        //       width: 200,
+        //       height: 100,
+        //     ),
+        //     Container(
+        //       margin: EdgeInsets.only(top: 20),
+        //       padding: EdgeInsets.all(40),
+        //       child: Text("This is Container 5", style: TextStyle(color: Colors.white),),
+        //       color: Colors.red,
+        //       width: 200,
+        //       height: 100,
+        //     ),
+        //     Container(
+        //       margin: EdgeInsets.only(top: 20),
+        //       padding: EdgeInsets.all(40),
+        //       child: Text("This is Container 6", style: TextStyle(color: Colors.white),),
+        //       color: Colors.green,
+        //       width: 200,
+        //       height: 100,
+        //     ),
+        //   ],
+        // ),
+        // ),
+
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Center(
+            child: ListView(
+              children: [
+
+                Container(
+                  width: 300,
+                  height: 300,
+                  color: Colors.blue[600],
+                  child: Icon(
+                    Icons.message,
+                    color: Colors.white,
+                    size: 100,
+                  ),
+                ),
+                Container(
+                  width: 300,
+                  height: 300,
+                  color: Colors.green[300],
+                  child: Icon(
+                    Icons.call,
+                    color: Colors.white,
+                    size: 100,
+                  ),
+                ),
+                Container(
+                  width: 300,
+                  height: 300,
+                  color: Colors.red[200],
+                  child: Icon(
+                    Icons.login,
+                    color: Colors.white,
+                    size: 100,
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+          )
       ),
     )
   );
