762d1a1c47e0f07c564ac7a68d1aada061c03020
Faraz Inam <farazinam@aptech.com>
2026-09-25 11:36:10 +0500

    Form Validation using Material UI

--- ADSE-I/React/frontend/src/metrialui/Contact.jsx
+++ ADSE-I/React/frontend/src/metrialui/Contact.jsx
@@ -9,28 +9,61 @@ function Contact() {
         sub: ""
     });
 
+    const [error, setError] = useState({});
+
     const HandleChange = (e) => {
         setData({...data, [e.target.name]: e.target.value})
     }
 
     const HandleSubmit = (e) => {
         e.preventDefault();
-        console.log("Form Submitted Successfully");
+
+        let temp = {};
+        if(!data.un) temp.username = "UserName Required";
+        if(!data.em) temp.email = "Email Required";
+        if(!data.sub) temp.subject = "Subject Required";
+
+        setError(temp);
+        
+        if(Object.keys(temp).length == 0){
+        alert("Form Submitted Successfully");
+        }
     }
   return (
-    <Container  sx={{display: 'flex', flexDirection: 'column', gap: 2, justifyContent: 'center'}}>
+    <Container  sx={{display: 'flex', flexDirection: 'column', 
+    gap: 2, justifyContent: 'center'}}>
         <Typography variant='h2'>
             Contact Us
         </Typography>
 
         <Box component='form' onSubmit={HandleSubmit}
-        sx={{display: 'flex', flexDirection: 'column', gap: 2, justifyContent: 'center'}}
+        sx={{display: 'flex', flexDirection: 'column', gap: 2, 
+            justifyContent: 'center'}}
         >
-            <TextField variant='outlined' label='UserName' name='un' value={data.un} onChange={HandleChange} required  />
+            <TextField 
+            variant='outlined' label='UserName' 
+            name='un' value={data.un} 
+            onChange={HandleChange}
+            error={!!error.username}
+            helperText={error.username}
+              />
 
-            <TextField variant='filled' label='Email' name='em' value={data.em} onChange={HandleChange} required  />
+            <TextField 
+            variant='filled' 
+            label='Email' name='em' 
+            value={data.em} 
+            onChange={HandleChange}
+            error={!!error.email}
+            helperText={error.email}
+            />
 
-            <TextField variant='standard' label='Subject' name='sub' value={data.ps} onChange={HandleChange} required  />
+            <TextField 
+            variant='standard' label='Subject' 
+            name='sub' value={data.sub} 
+            onChange={HandleChange}
+            error={!!error.subject}
+            helperText={error.subject}
+            />
 
             <Button type='submit' variant='contained'> Submit </Button>
         </Box>
